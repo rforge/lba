fe16d714f8db3322265eb1c259122fda80981a5a
ingmarvisser <ingmarvisser@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-03-28 19:52:24 +0000

    =Added vectorized version of rlba: rbla2
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lba@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/glba/R/lba.R
+++ pkg/glba/R/lba.R
@@ -2,337 +2,346 @@
 
 lba <-
 function(rt, 
-	response, 
-	data, 
-	weights,
-	sddr = ~1, sp = ~1, bound = ~1, nond = ~1, drift = ~1,
-	scaling = "sum",
-	loglink = c(FALSE,FALSE,FALSE,FALSE),
-	sdstart = 0.1,
-	startmethod="smartstart",
-	nstart = 100,
-	nondecconstr = TRUE,
-	startpars = NULL,
-	fixed = NULL,
-	method = "L-BFGS-B",
-	hessian = FALSE,
-	lower = -Inf,
-	upper = Inf, 
-	fit=TRUE, 
-	debug=FALSE) {
-	
-	call <- match.call()
-	
-	if(missing(data)) stop("'data' cannot be missing in a data analysis routine")
-	
-	nn <- nrow(data)
-	nm <- deparse(substitute(rt))
+    response, 
+    data, 
+    weights,
+    sddr = ~1, sp = ~1, bound = ~1, nond = ~1, drift = ~1,
+    scaling = "sum",
+    loglink = c(FALSE,FALSE,FALSE,FALSE),
+    sdstart = 0.1,
+    startmethod="smartstart",
+    nstart = 100,
+    nondecconstr = TRUE,
+    startpars = NULL,
+    fixed = NULL,
+    method = "L-BFGS-B",
+    hessian = FALSE,
+    lower = -Inf,
+    upper = Inf, 
+    fit=TRUE, 
+    debug=FALSE) {
+    
+    call <- match.call()
+    
+    if(missing(data)) stop("'data' cannot be missing in a data analysis routine")
+    
+    # extract rt and resp from data
+    nn <- nrow(data)
+    nm <- deparse(substitute(rt))
+    form <- as.formula(paste(nm,"~1",sep=""))
+    rt <- model.frame(form,data=data)[,1]
+    nm <- deparse(substitute(response))
+    form <- as.formula(paste(nm,"~1",sep=""))
+    resp <- model.frame(form,data=data)[,1]
+    
+    ndrift <- length(unique(resp))
+    ncat <- length(unique(resp))
+    
+    # specify weights or get them from data
+    if(missing(weights)) weights <- rep(1,nn)
+    else {
+	nm <- deparse(substitute(weights))
 	form <- as.formula(paste(nm,"~1",sep=""))
-	rt <- model.frame(form,data=data)[,1]
-	nm <- deparse(substitute(response))
-	form <- as.formula(paste(nm,"~1",sep=""))
-	resp <- model.frame(form,data=data)[,1]
+	weights <- model.frame(form,data=data)[,1]
+    }
+    
+    # sanity checks on provided rt, resp, weights
+    if(length(weights)!=nn) stop("'weights' does not have correct length")
+    if(length(rt)!=nn) stop("'rt' does not have correct length")
+    if(length(resp)!=nn) stop("'resp' does not have correct length")
+    
+    # create submodels from provided formulae
+    sddrmod <- pmod(sddr, data=data, prefix="sddr")
+    spmod <- pmod(sp, data=data, prefix="sp")
+    boundmod <- pmod(bound, data=data, prefix="bound")
+    nondmod <- pmod(nond, data=data, prefix="nond")
+    
+    # create drift rate model(s)
+    multiDrift <- FALSE
+    if(is.list(drift)) {
+	multiDrift <- TRUE
+	ndriftModels <- length(drift)
+	if(ndriftModels!=ncat) stop("Nr of 'drift' rate models should equal the number of categories in the 'response' variable")
+	driftModels <- list()
+	for(i in 1:ncat) {
+	    driftModels[[i]] <- pmod(drift[[i]], data=data, prefix=paste("drift",i,sep=""))
+	}
+    } else {
+	drmod <- pmod(drift, data=data, prefix="drift")
+    }
+    
+    # create list of all submodels
+    if(multiDrift) {
+	models <- list(sddrmod,spmod,boundmod,nondmod)
+	for(i in 1:ncat) {
+	    models[[4+i]] <- driftModels[[i]]
+	}
+    } else {
+	models <- list(sddrmod,spmod,boundmod,nondmod,drmod)
+    }
+    
+    # get the lengths of parameter vectors
+    npars <- unlist(lapply(models,function(x){length(getPars(x))}))
+    
+    if(debug) {
+	cat("npars: ", npars)
+    }
+   
+    # get begin and end indices of submodel parameters
+    lt <- length(npars)
+    et <- cumsum(npars)
+    bt <- c(1,et[-lt]+1)
+
+    # checks on the fixed vector if provided ...
+    # ... or else create one with default options (none fixed unless scaling==fixedSD)
+    if(!(is.null(fixed))) {
+	lf <- length(fixed)
+	if(!(lf==sum(npars))) stop(paste("'fixed' has incorrect length, should be ", sum(npars)))
+	if(is.null(startpars)) stop("'fixed' parameters can only be provided in combinations with starting values")
+	if(!(is.logical(fixed))) stop("'fixed' must be of type logical")
+	if(scaling=="fixedSD"&!fixed[1]==TRUE) {
+	    warning("Sd of drift rate has been set to fixed value (default = 0.1).")
+	    fixed[1] <- TRUE
+	    startpars[1] <- sdstart
+	}
+    } else {
+	fixed <- rep(FALSE, sum(npars))
+	if(scaling=="fixedSD") {
+	    fixed[1] <- TRUE
+	    startpars[1] <- sdstart
+	}
+    }
+    
+    # get starting values from the models
+    # why do this? starting values have not been set in the submodels ...
+    allpars <- unlist(lapply(models,getPars))
+    
+    parsMat <- matrix(,ncol=4+ncat,nrow=nn)
+    
+    # define logl function to be optimized
+    logl <- function(pars) {
 	
-	ndrift <- length(unique(resp))
-	ncat <- length(unique(resp))
+	# include fixed pars
+	allpars[!fixed] <- pars
 	
-	if(missing(weights)) weights <- rep(1,nn)
-	else if(!missing(data)) {
-		nm <- deparse(substitute(weights))
-		form <- as.formula(paste(nm,"~1",sep=""))
-		weights <- model.frame(form,data=data)[,1]
+	# uncomment to set the start point to a fraction of the boundary
+	# allpars[2] <- 0.5*allpars[3]
+	
+	# expand pars to matrix
+	for(i in 1:4) {
+	    parsMat[,i] <- predpmod(models[[i]],allpars[bt[i]:et[i]])
 	}
 	
-	if(length(weights)!=nn) stop("'weights' does not have correct length")
-	if(length(rt)!=nn) stop("'rt' does not have correct length")
-	if(length(resp)!=nn) stop("'resp' does not have correct length")
-	
-	# create submodels 
-	sddrmod <- pmod(sddr, data=data, prefix="sddr")
-	spmod <- pmod(sp, data=data, prefix="sp")
-	boundmod <- pmod(bound, data=data, prefix="bound")
-	nondmod <- pmod(nond, data=data, prefix="nond")
-	multiDrift <- FALSE
-	if(is.list(drift)) {
-	    multiDrift <- TRUE
-	    ndriftModels <- length(drift)
-	    if(ndriftModels!=ncat) stop("Nr of 'drift' rate models should equal the number of categories in the 'response' variable")
-	    driftModels <- list()
-	    for(i in 1:ncat) {
-		driftModels[[i]] <- pmod(drift[[i]], data=data, prefix=paste("drift",i,sep=""))
-	    }
-	} else {
-	    drmod <- pmod(drift, data=data, prefix="drift")
-	}
-	
+	# here we need predictions for the drift rate for all possible responses, 
+	# not just the actual response		
 	if(multiDrift) {
-	    models <- list(sddrmod,spmod,boundmod,nondmod)
 	    for(i in 1:ncat) {
-		models[[4+i]] <- driftModels[[i]]
+		parsMat[,4+i] <- predpmod(models[[4+i]],allpars[bt[4+i]:et[4+i]])
 	    }
 	} else {
-	    models <- list(sddrmod,spmod,boundmod,nondmod,drmod)
+	    parsMat[,5] <- predpmod(models[[5]],allpars[bt[5]:et[5]])
+	    parsMat[,6] <- 1-parsMat[,5]
 	}
 	
-	npars <- unlist(lapply(models,function(x){length(getPars(x))}))
+	# reorder the drift parameters
+	parsMat[,5:(4+ncat)] <- reorderDrift(resp,parsMat[,5:(4+ndrift)])
 	
-	if(debug) {
-	    cat("npars: ", npars)
+	ll <- obj(rt,parsMat,loglink=loglink,weights=weights)
+	
+	if(debug) print(head(parsMat,10))
+	
+	safe=TRUE
+	
+	if(safe) {
+	    if(is.infinite(ll)) ll <- -1e10
+	    if(is.nan(ll)) ll <- -1e10
 	}
 	
-	if(!(is.null(fixed))) {
-	    lf <- length(fixed)
-	    if(!(lf==sum(npars))) stop(paste("'fixed' has incorrect length, should be ", sum(npars)))
-	    if(is.null(startpars)) stop("'fixed' parameters can only be provided in combinations with starting values")
-	    if(!(is.logical(fixed))) stop("'fixed' must be of type logical")
-	    if(scaling=="fixedSD"&!fixed[1]==TRUE) {
-		warning("Sd of drift rate has been set to fixed value (default = 0.1).")
-		fixed[1] <- TRUE
-		startpars[1] <- sdstart
-	    }
-	} else {
-	    fixed <- rep(FALSE, sum(npars))
-	    if(scaling=="fixedSD") {
-		fixed[1] <- TRUE
-		startpars[1] <- sdstart
-	    }
-	}
+	if(debug) print(ll)
 	
-	# get starting values from the models
-	allpars <- unlist(lapply(models,getPars))
-	# get starting values if provided
-	if(!is.null(startpars)) {
-	    namesp <- names(allpars)
-	    allpars <- startpars
-	    names(allpars) <- namesp
-	} else {
-	    # generate random start values 
-	    namesp <- names(allpars)
-	    allpars <- runif(length(allpars))
-	    names(allpars) <- namesp
-	}
-		
-	# get begin and end indices of submodel parameters
-	lt <- length(npars)
-	et <- cumsum(npars)
-	bt <- c(1,et[-lt]+1)
-	
-	parsMat <- matrix(,ncol=4+ncat,nrow=nn)
-	
-	# define logl function to be optimized
-	logl <- function(pars) {
-		# include fixed pars
-		allpars[!fixed] <- pars
-		
-# 		uncomment to set the start point to a fraction of the boundary
-# 		allpars[2] <- 0.5*allpars[3]
-		
-		# expand pars to matrix
-		for(i in 1:4) {
-			parsMat[,i] <- predpmod(models[[i]],allpars[bt[i]:et[i]])
-		}
-		
-		# here we need predictions for the drift rate for all possible responses, 
-		# not just the actual response		
-		if(multiDrift) {
-			for(i in 1:ncat) {
-				parsMat[,4+i] <- predpmod(models[[4+i]],allpars[bt[4+i]:et[4+i]])
-			}
-		} else {
-			parsMat[,5] <- predpmod(models[[5]],allpars[bt[5]:et[5]])
-			parsMat[,6] <- 1-parsMat[,5]
-		}
-		
-		# reorder the drift parameters
-		parsMat[,5:(4+ncat)] <- reorderDrift(resp,parsMat[,5:(4+ndrift)])
-		
-		ll <- obj(rt,parsMat,loglink=loglink,weights=weights)
-		
-  		if(debug) print(head(parsMat,10))
-		
-		safe=TRUE
-		
-		if(safe) {
-		    if(is.infinite(ll)) ll <- -1e10
-		    if(is.nan(ll)) ll <- -1e10
-		}
-		
-		if(debug) print(ll)
-		
-		return(ll)
-	}
-		
-	pars <- allpars[!fixed]
-	
-	if(debug) {
-		print("Initial parameters and log-likelihood")
-		print(pars)
-		print(logl(pars))
-	}
-	
-	npp <- sum(npars)
+	return(ll)
+    }
+    
+    #
+    # STARTING values
+    #
+    
+    # get starting values if provided
+    if(!is.null(startpars)) {
+	namesp <- names(allpars)
+	allpars <- startpars
+	names(allpars) <- namesp
+	startmethod <- "user"
+    } else {
+	# generate random start values 
+	namesp <- names(allpars)
+	allpars <- runif(length(allpars))
+	names(allpars) <- namesp
+    }
+    
+    npp <- sum(npars)
+        
+    if(startmethod=="random") {
 	
 	lls <- numeric(nstart)
-	if(startmethod=="random") {
-	    parsvec <- runif(npp*nstart)
-# 	    print(parsvec)
-# 	    print(nstart)
-	    for(i in 1:nstart) {
-		allpars <- parsvec[1:npp+(i-1)*npp]
-		pars <- allpars[!fixed]
-#  		print(pars)
-		lls[i] <- logl(pars)
-	    }
-	    bestll <- which.max(lls)
-	    allpars <- parsvec[1:npp+(bestll-1)*npp]
-	    names(allpars) <- namesp
+	parsvec <- runif(npp*nstart)
+	for(i in 1:nstart) {
+	    allpars <- parsvec[1:npp+(i-1)*npp]
+	    pars <- allpars[!fixed]
+	    #  		print(pars)
+	    lls[i] <- logl(pars)
 	}
+	bestll <- which.max(lls)
+	allpars <- parsvec[1:npp+(bestll-1)*npp]
+	names(allpars) <- namesp
+    }
+    
+    if(startmethod=="smartstart") {
 	
-	if(startmethod=="smartstart") {
-	    startp <- startlba(rt, resp)
-	    parsvec <- matrix(rnorm(npp*nstart,sd=0.05),ncol=npp,nrow=nstart)
-	    colnames(parsvec) <- namesp
-	    for(i in c(1,2,3,5)) parsvec[,bt[i]] <- runif(nstart,0.7*startp[i],startp[i])
-	    parsvec[,bt[4]] <- runif(nstart,0.5*startp[4],1.5*startp[4])
-	    
-	    colnames(parsvec) <- namesp
-	    
-	    allpars <- numeric(npp)
-	    allpars[bt] <- startp
-	    parsvec[1,] <- allpars
-	    
-	    lls <- numeric(nstart)	    
-	    names(allpars) <- namesp
-	    for(i in 1:nstart) {
-		allpars <- parsvec[i,]
-		pars <- allpars[!fixed]
-		lls[i] <- logl(pars)
-	    }
-	    
-# 	    print(lls)
-	    
-	    bestll <- which.max(lls)
-	    allpars <- parsvec[bestll,]
-	    names(allpars) <- namesp
-
-# 	    print(parsvec)
-	}	
+	startp <- startlba(rt, resp)
+	parsvec <- matrix(rnorm(npp*nstart,sd=0.05),ncol=npp,nrow=nstart)
+	colnames(parsvec) <- namesp
+	for(i in c(1,2,3,5)) parsvec[,bt[i]] <- runif(nstart,0.7*startp[i],startp[i])
+	parsvec[,bt[4]] <- runif(nstart,0.5*startp[4],1.5*startp[4])
 	
-	pars <- allpars[!fixed]
+	colnames(parsvec) <- namesp
 	
-	startpars <- allpars
+	allpars <- numeric(npp)
+	allpars[bt] <- startp
+	parsvec[1,] <- allpars
 	
-	initlogl <- logl(pars)
+	lls <- numeric(nstart)	    
+	names(allpars) <- namesp
+	for(i in 1:nstart) {
+	    allpars <- parsvec[i,]
+	    pars <- allpars[!fixed]
+	    lls[i] <- logl(pars)
+	}
+		
+	bestll <- which.max(lls)
+	allpars <- parsvec[bestll,]
+	names(allpars) <- namesp
+    }
+    
+    # set pars to startpars
+    startpars <- allpars
+    
+    pars <- allpars[!fixed]
+    
+    initlogl <- logl(pars)
+    
+    print(-initlogl)
+    
+    if(initlogl==-1e10) fit <- FALSE
+ 
+    pars <- allpars[!fixed]
+    
+    if(debug) {
+	print("Initial parameters and log-likelihood")
+	print(pars)
+	print(logl(pars))
+    }
+    
+    # 	lower <- c(0,0,0,0,0)
+    # 	upper <- c(10,10,10,.95,.95)
+    
+    #
+    # fit function
+    #  
+    
+    if(fit) {
 	
- 	print(-initlogl)
-	
-	if(initlogl==-1e10) fit <- FALSE
-	
-	# 	lower <- c(0,0,0,0,0)
-	# 	upper <- c(10,10,10,.95,.95)
-
-	if(fit) {
-	    if(!is.null(lower)|!is.null(upper)) {
-		if(method!="L-BFGS-B") {
-		    warning("parameter bounds (lower and upper) can only be used with method 'L-BFGS-B'; bounds are ignored.")
-		}
-	    }
-	    
-# 	    # constrain sum of nond pars to be larger than zero
-# 	    uinond <- matrix(0,nrow=1,ncol=npp)
-# 	    uinond[1,bt[4]:et[4]] <- 1
-# 	    
-# 	    ui <- diag(npp)
-# 	    ui <- rbind(ui,uinond)
-# 	    
-# 	    ui <- ui[,!fixed] 
-# 	    
-# 	    ci <- rep(-Inf,length(pars))
-# 	    ci[bt] <- 0
-# 	    
-# 	    ci <- c(ci,0)
-# 	    
-# 	    print(ui)
-# 	    print(ui%*%pars)
-# 	    print(ci)
-	    
-	    if(method=="L-BFGS-B") {
-		lower <- rep(-Inf,length(pars))
-		# lower <- rep(0,length(pars))
-		lower[bt] <- 0
-		if(nondecconstr) lower[bt[4]:et[4]] <- 0
-		upper <- rep(Inf,length(pars))
-	    }
-	    
-	    maxit <- 1000
-	    if(method=="Nelder-Mead") maxit <- 10000
-	    
-	    # fit the model
-	    res <- optim(fn=logl,par=pars,
-		method=method,
-		hessian=FALSE,
- 		lower=lower,
- 		upper=upper,
-# 		ui=ui,
-# 		ci=ci,
-		control=list(maxit=maxit,trace=1,fnscale=-1))
-	    
-	    allpars[!fixed] <- res$par
-	    res$par <- allpars
-	    
-	    # set sp to it's appropriate value
-	    # res$par[2] <- 0.5*res$par[3]
-	    
-	    if(res$convergence==0 && res$value!=-1e10) {
-		if(hessian) {
-		    res$hessian <- optimHess(fn=logl,par=allpars[!fixed])
-		    res$hessian <- -1*res$hessian # needed because of maximization done instead of minimization
-		    res <- res[c("par","value","convergence","hessian")]
-		    info <- try(solve(res$hessian),silent=TRUE)
-		    if(class(info)=="try-error") res$ses <- NULL
-		    else res$ses <- sqrt(diag(info))			
-		} else res <- res[c("par","value","convergence")]
-	    } else {
-		res <- res[c("par","value","convergence","message")]
-		warning("Likelihood optimization did not converge with code ", res$convergence, " and message %s ", res$message)
-	    }
-	    
-	    names(res)[1:2] <- c("pars","logl")
-	    
-	    for(i in 1:length(npars)) {
-		models[[i]] <- setPars(models[[i]],res$par[bt[i]:et[i]])
+	if(!is.null(lower)|!is.null(upper)) {
+	    if(method!="L-BFGS-B") {
+		warning("parameter bounds (lower and upper) can only be used with method 'L-BFGS-B'; bounds are ignored.")
 	    }
 	}
 	
-	if(!fit) {
-	    res <- list()
-	    res$logl <- initlogl
-	    if(initlogl==-1e10) {
-		res$message="non-feasible start vals"
-		res$convergence <- 1
-	    }
+	# constrain sum of nond pars to be larger than zero
+	# uinond <- matrix(0,nrow=1,ncol=npp)
+	# uinond[1,bt[4]:et[4]] <- 1
+	#  
+	# ui <- diag(npp)
+	# ui <- rbind(ui,uinond)
+	# 	    
+	# ui <- ui[,!fixed] 
+	# 	    
+	# ci <- rep(-Inf,length(pars))
+	# ci[bt] <- 0
+	# 	    
+	# ci <- c(ci,0)
+	
+	if(method=="L-BFGS-B") {
+	    lower <- rep(-Inf,length(pars))
+	    # lower <- rep(0,length(pars))
+	    lower[bt] <- 0
+	    if(nondecconstr) lower[bt[4]:et[4]] <- 0
+	    upper <- rep(Inf,length(pars))
 	}
 	
-	# add fixed to res
-	res$fixed <- fixed
+	maxit <- 1000
 	
-	# add models to res
-	res$model <- models
+	if(method=="Nelder-Mead") maxit <- 10000
 	
-	# add npars to res
-	res$npar <- sum(npars)
-	res$freepars <- sum(npars)-sum(fixed)
+	# fit the model
+	res <- optim(fn=logl,par=pars,
+	    method=method,
+	    hessian=FALSE,
+	    lower=lower,
+	    upper=upper,
+	    # ui=ui,
+	    # ci=ci,
+	    control=list(maxit=maxit,trace=1,fnscale=-1)
+	)
 	
-	# add nr of data for use in bic
-	res$nobs <- length(rt)
+	allpars[!fixed] <- res$par
+	res$par <- allpars
 	
-	res$call <- call
+	# set sp to it's appropriate value
+	# res$par[2] <- 0.5*res$par[3]
 	
-	res$startpars <- startpars
+	# add hessian, select outputs dependent on convergence
+	if(res$convergence==0 && res$value!=-1e10) {
+	    if(hessian) {
+		res$hessian <- optimHess(fn=logl,par=allpars[!fixed])
+		res$hessian <- -1*res$hessian # needed because of maximization done instead of minimization
+		res <- res[c("par","value","convergence","hessian")]
+		info <- try(solve(res$hessian),silent=TRUE)
+		if(class(info)=="try-error") res$ses <- NULL
+		else res$ses <- sqrt(diag(info))			
+	    } else res <- res[c("par","value","convergence")]
+	} else {
+	    res <- res[c("par","value","convergence","message")]
+	    warning("Likelihood optimization did not converge with code ", res$convergence, " and message %s ", res$message)
+	}
 	
-	class(res) <- "lba"
+	names(res)[1:2] <- c("pars","logl")
 	
-	return(res)
+	for(i in 1:length(npars)) {
+	    models[[i]] <- setPars(models[[i]],res$par[bt[i]:et[i]])
+	}
+    }
+    
+    if(!fit) {
+	res <- list()
+	res$logl <- initlogl
+	if(initlogl==-1e10) {
+	    res$message="non-feasible start values"
+	    res$convergence <- 1
+	}
+    }
+    
+    # add fixed, models, npars, nobs, call and startpars to res
+    res$fixed <- fixed
+    res$model <- models
+    res$npar <- sum(npars)
+    res$freepars <- sum(npars)-sum(fixed)
+    res$nobs <- length(rt)
+    res$call <- call
+    res$startpars <- startpars
+    
+    class(res) <- "lba"
+    return(res)
 }
 
 print.lba <- function(x, ...) {
@@ -399,7 +408,7 @@ logLik.lba <- function(object, ...) {
 
 tablba <- function(object) {
 	
-    if(!"hessian" %in% names(object))stop("Hessian required to compute standard errors; set hessian=TRUE when fitting the model")
+    if(!"hessian" %in% names(object)) stop("Hessian required to compute standard errors; set hessian=TRUE when fitting the model")
     
     pp <- object$pars
     ses <- rep(0,length(pp))
--- pkg/glba/R/rlba.R
+++ pkg/glba/R/rlba.R
@@ -1,20 +1,60 @@
+
 rlba <-
 function(n,b,A,vs,s,t0,st0=0,truncdrifts=TRUE){
-	n.with.extras=ceiling(n*(1+3*prod(pnorm(-vs))))
-	drifts=matrix(rnorm(mean=vs,sd=s,n=n.with.extras*length(vs)),ncol=length(vs),byrow=TRUE)
-	if (truncdrifts) {
-		repeat {
-			drifts=rbind(drifts,matrix(rnorm(mean=vs,sd=s,n=n.with.extras*length(vs)),ncol=length(vs),byrow=TRUE))
-			tmp=apply(drifts,1,function(x) any(x>0))
-			drifts=drifts[tmp,]
-			if (nrow(drifts)>=n) break
-		}
+    n.with.extras=ceiling(n*(1+3*prod(pnorm(-vs))))
+    drifts=matrix(rnorm(mean=vs,sd=s,n=n.with.extras*length(vs)),ncol=length(vs),byrow=TRUE)
+    if(truncdrifts) {
+	repeat {
+	    drifts=rbind(drifts,matrix(rnorm(mean=vs,sd=s,n=n.with.extras*length(vs)),ncol=length(vs),byrow=TRUE))
+	    tmp=apply(drifts,1,function(x) any(x>0))
+	    drifts=drifts[tmp,]
+	    if (nrow(drifts)>=n) break
 	}
-	drifts=drifts[1:n,]
-	drifts[drifts<0]=0
-	starts=matrix(runif(min=0,max=A,n=n*length(vs)),ncol=length(vs),byrow=TRUE)
-	ttf=t((b-t(starts)))/drifts
-	rt=apply(ttf,1,min)+t0+runif(min=-st0/2,max=+st0/2,n=n)
-	resp=apply(ttf,1,which.min)
-	data.frame(resp=resp,rt=rt)
+    }
+    drifts=drifts[1:n,]
+    drifts[drifts<0]=0
+    starts=matrix(runif(min=0,max=A,n=n*length(vs)),ncol=length(vs),byrow=TRUE)
+    ttf=t((b-t(starts)))/drifts
+    rt=apply(ttf,1,min)+t0+runif(min=-st0/2,max=+st0/2,n=n)
+    resp=apply(ttf,1,which.min)
+    data.frame(resp=resp,rt=rt)
+}
+
+
+# vectorized in all arguments compared to rlba
+# suitable checks
+# zap drifts smaller than 0
+rlba2 <-
+function(nn,bnd,sp,drift,sddr,nond,st0=0) {
+    
+    if(is.matrix(drift)) {
+	if(nn==nrow(drift)) {
+	    ncat <- ncol(drift)
+	    ndrfts <- ncat*nn
+	    if(length(sddr)==nn) sddr <- rep(sddr,each=ncat)
+	    else if(length(sddr)!=1) stop("'sddr' has incorrect length, should be 1 or nrow(drift)")
+	    drfts <- matrix(rnorm(ndrfts,mean=c(t(drift)),sd=sddr),ncol=ncat,byrow=TRUE)
+	} else {
+	    stop("'nn' should be equal to nrow(drift) when drift is specified as matrix in rlba2.")
+	}
+    } else {
+	ncat <- length(drift)
+	ndrfts <- ncat*nn
+	drfts <- matrix(rnorm(ndrfts,mean=c(drift),sd=sddr),ncol=ncat,byrow=TRUE)
+    }
+    
+    drfts[drfts<0]=0
+    
+#     print(head(drfts))
+#     print(dim(drfts))
+    
+    starts=matrix(runif(min=0,max=sp,n=nn*ncat),ncol=ncat,byrow=TRUE)
+    
+#     print(head(starts))
+#     print(dim(starts))
+    
+    ttf=t((bnd-t(starts)))/drfts
+    rt=apply(ttf,1,min)+nond+runif(min=-st0/2,max=+st0/2,n=nn)
+    resp=apply(ttf,1,which.min)
+    data.frame(resp=resp,rt=rt)
 }
